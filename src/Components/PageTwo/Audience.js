b5f87544241820107a3c22c275f233d880870ac3
Seweryn Kuliga <skuligas@gmail.com>
2021-12-27 14:58:15 +0100

    Update
    Refresh project

--- src/Components/PageTwo/Audience.js
+++ src/Components/PageTwo/Audience.js
@@ -1,49 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useState, useContext, useEffect } from "react";
-import { ReservationContext } from '../../Contexts/ReservationContext';
-
+import React from "react";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 function Audience({ reserved }) {
-    const { seats, setSeats } = useContext(ReservationContext);
-    const [chosen, setChosenSeats] = useState([])
-    const [pickedPlace, setPickedPlace] = useState(false)
+  const [pickedPlace, setPickedPlace] = useState(false);
 
-    const handleChange = (e) => {
-        setPickedPlace(!pickedPlace);
-        setChosenSeats({ 'id': e.target.value })
-    }
+  const handleChange = () => {
+    setPickedPlace(!pickedPlace);
+  };
 
-    // const handleChange1 = (e) => {
-    //     setChosenSeats(e.target.props.id)
-    // }
+  useEffect(() => {
+    setPickedPlace(pickedPlace);
+  }, [pickedPlace]);
 
-    // const chosen = seats.slice(reserved)
-
-    useEffect(() => {
-        setPickedPlace(pickedPlace)
-    }, [pickedPlace]);
-
-
-    return (
-        <Seat
-            reserved={reserved}
-            className={pickedPlace ? "picked" : "auto"}
-            onClick={handleChange}
-        // onChange={handleChange1}
-        >
-        </Seat>
-    )
+  return (
+    <Seat
+      reserved={reserved}
+      className={pickedPlace ? "picked" : "auto"}
+      onClick={handleChange}
+    ></Seat>
+  );
 }
 
 export default Audience;
 
 const Seat = styled.span`
-    display: inline;
-    position: relative;
-    margin: 5px;
-    border: 1px solid;
-    width: 55px;
-    height: 55px;
-    background-color: ${props => props.reserved ? 'rgb(29, 21, 7)' : 'auto'};
-    `
+  display: inline;
+  position: relative;
+  margin: 5px;
+  border: 1px solid;
+  width: 55px;
+  height: 55px;
+  background-color: ${(props) => (props.reserved ? "rgb(29, 21, 7)" : "auto")};
+`;
--- src/Components/PageTwo/PageTwo.js
+++ src/Components/PageTwo/PageTwo.js
@@ -1,59 +1,54 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useContext } from "react";
-import Audience from './Audience';
-import styled from 'styled-components';
-import Legend from './Legend';
-import CinemaScreen from './CinemaScreen';
-import { ReservationContext } from '../../Contexts/ReservationContext';
-import '../../App.css';
-
+import Audience from "./Audience";
+import styled from "styled-components";
+import Legend from "./Legend";
+import CinemaScreen from "./CinemaScreen";
+import { ReservationContext } from "../../Contexts/ReservationContext";
+import "../../App.css";
 
 function PageTwo() {
-
-
   const { seats, setSeats } = useContext(ReservationContext);
-
-
-
   const fetchSeats = async () => {
-    const response = await fetch('http://localhost:3001/seats');
+    const response = await fetch("http://localhost:3001/seats");
     const data = await response.json();
     console.log(data);
     setSeats(data);
-  }
+  };
 
   useEffect(() => {
-    fetchSeats()
-  }, [])
+    fetchSeats();
+  }, []);
 
   return (
     <div>
       <CinemaScreen />
       <Container>
-        {seats && seats.map(seats => <Audience
-          id={seats.id}
-          key={seats.id}
-          reserved={seats.reserved}
-        />
-        )}
+        {seats &&
+          seats.map((seats) => (
+            <Audience id={seats.id} key={seats.id} reserved={seats.reserved} />
+          ))}
       </Container>
       <Legend />
 
       <Message>
-            <h1>Twoja rezerwacja przebiegła pomyślnie!</h1>
-            <br />
-            <h3>Wybrałeś miejsca:</h3>
-            {/* tablica.sort() - sortuje elementy */}
-            <div>
-                {/* {chosenSeats.map((seat) => {
+        <h1>Twoja rezerwacja przebiegła pomyślnie!</h1>
+        <br />
+        <h3>Wybrałeś miejsca:</h3>
+        {/* tablica.sort() - sortuje elementy */}
+        <div>
+          {/* {chosenSeats.map((seat) => {
                     return <ChosenSeats />
                 })} */}
-            </div>
-            <br />
-            <h3>Dziękujemy! W razie problemów prosimy o kontakt z działem administracji.</h3>
-        </Message>
+        </div>
+        <br />
+        <h3>
+          Dziękujemy! W razie problemów prosimy o kontakt z działem
+          administracji.
+        </h3>
+      </Message>
     </div>
-  )
+  );
 }
 
 export default PageTwo;
@@ -74,8 +69,8 @@ const Container = styled.div`
   border: 1px solid;
   display: grid;
   grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto;
-`
+`;
 
 const Message = styled.div`
-    margin: 10% 10% 10% 20%;
-`
+  margin: 10% 10% 10% 20%;
+`;
